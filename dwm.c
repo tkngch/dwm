615c2cea67b88593e1ca53d728528a6e53e8b1aa
Takao Noguchi <tkngch@runbox.com>
2020-04-03 21:40:04 +0100

    make the tag indication box bigger

--- dwm.c
+++ dwm.c
@@ -697,7 +697,7 @@ drawbar(Monitor *m)
 {
 	int x, w, sw = 0;
 	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
+	int boxw = drw->fonts->h / 6 + 3;
 	unsigned int i, occ = 0, urg = 0;
         unsigned int n = 0;
         char layout_indication[16];
